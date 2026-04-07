81c2365ba8682f9f27ad626f477fc922c6fc9cf1
LlucVelazquez <lluc.velazquez.7e8@itb.cat>
2026-04-07 19:27:53 +0200

    feat: cooldown bullet

--- ProjecteFinal-Roguelite/Assets/_Scripts/Behaviours/ProjectileFiringBehaviour.cs
+++ ProjecteFinal-Roguelite/Assets/_Scripts/Behaviours/ProjectileFiringBehaviour.cs
@@ -4,13 +4,18 @@ namespace Roguelite.Behaviours
 {
     public class ProjectileFiringBehaviour : MonoBehaviour
     {
+        public float fireRate = 1f;
+        private float _nextFireTime = 0f;
         public void FireProjectile(GameObject projectilePrefab, Transform spawner, bool fire)
         {
-            if (fire)
+            if (fire && Time.time >= _nextFireTime)
             {
+                _nextFireTime = Time.time + fireRate;
+
                 GameObject projectile = Instantiate(projectilePrefab);
                 projectile.transform.position = spawner.position;
                 projectile.transform.rotation = transform.rotation;
+                Destroy(projectile, 2f);    
             }
         }
     }
