a684c2315e3cbe36a3b4b6f2940ef81a19e3cb7c
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2026-05-12 16:51:55 +0200

    feat: started enemy spawn

---
+++ ProjecteFinal-Roguelite/Assets/_Scripts/EnemySpawner.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Roguelite
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _skeletions;
+        [SerializeField] private GameObject[] _meleeWeapons;
+        [SerializeField] private GameObject[] _rangedWeapons;
+
+        private void SpawnSkeleton()
+        {
+            
+        }
+    }
+}
